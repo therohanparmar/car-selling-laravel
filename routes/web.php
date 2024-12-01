5e3ed7d7afda318134567fce44378d20c54e06cc
Rohan Parmar <rohanrparmar987@gmail.com>
2024-12-01 14:50:05 +0530

    Website Layout with Template Inheritance :page_facing_up:

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/signup', [SignupController::class, 'create']);
+Route::get('/login', [LoginController::class, 'create']);
